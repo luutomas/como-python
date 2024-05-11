54c8fd927ef3f0f0433093983c9e99dd620b0405
Jan Zuska <jan.zuska.04@gmail.com>
2024-05-11 12:12:51 +0200

    Ukol na multiselect

--- pages/lekce_6.py
+++ pages/lekce_6.py
@@ -275,7 +275,7 @@ Na závěr vypište vybrané jídlo a nápoj.
 Na základě vybraného jídla a nápoje vypište celkovou cenu objednávky.
             
 ### Úkol 4
-Do jsonu přidejte další kategorii `typ platby` a do ní jako hondontu seznam možných plateb (hotově, kartou, kupon). \\
+Do jsonu přidejte další kategorii `typ platby` a do ní jako hodnotu seznam možných plateb (hotově, kartou, kupon). \\
 Aktualizujte aplikaci tak, aby uživatel mohl vybrat i typ platby a vypište vybraný typ platby.
             
 ### Úkol 5
